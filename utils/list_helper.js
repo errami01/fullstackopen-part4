c75558b36a190e95f56b64b245943ec501673215
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-11 20:09:51 +0100

    Add 4.5: Helper Functions and Unit Tests, step 3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs)=>{
         return total + blog.likes
     },0) 
 }
+const favoriteBlog =(blogs)=>{
+    return blogs.reduce((a, b)=>{
+        return a.likes >= b.likes ? a : b
+    }, 0)
+}
 module.exports={
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
