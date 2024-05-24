c0c9c2b5c6769c9bd3b890d92b34fed66b1f7002
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-24 20:04:39 +0100

    Add 4.18: Blog List Expansion, step 6

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -15,6 +16,7 @@ mongoose.connect(mongoUrl)
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 module.exports = app
