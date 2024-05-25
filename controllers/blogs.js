3d38c8933a1bde3bf57120f6482713550e024f00
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-25 12:33:28 +0100

    Add 4.20*: Blog List Expansion, step 8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,13 +3,6 @@ const Blog = require('../models/blog.js')
 const User = require('../models/user.js')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user',{blogs:0})
@@ -17,7 +10,7 @@ blogsRouter.get('/', async (request, response) => {
 })
   
 blogsRouter.post('/', async (request, response, next) => {
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
